7f20602e744b06fef2dde6b5e9eafd9cfc459822
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2026-07-21 14:49:07 +0200

    added mongodb with docker setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
+
 const port = process.env.PORT || 5000;
+
+mongoose.connect("mongodb://localhost:27017/neighborhood-surveillance")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.get('/', (req, res) => {
   res.send('neighborhood Surveillance is running!');
@@ -9,3 +20,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+

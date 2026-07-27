af5905ba280a28849126b8a44091b8b3c160ed6d
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2026-07-27 15:56:07 +0200

    added neighbourhood zone system

--- backend/src/routes/zoneRoutes.js
+++ backend/src/routes/zoneRoutes.js
@@ -13,4 +13,70 @@ router.get("/", async (req, res) => {
 	}
 });
 
+router.post("/seed", async (req, res) => {
+
+    try {
+        await Zone.deleteMany();
+
+        const zones = await Zone.insertMany([
+            {   name: "A1" ,
+                locations: []
+            },
+            {   name: "A2" ,
+                locations: []
+            },
+            {   name: "A3" ,
+                locations: []
+            },
+            {   name: "A4" ,
+                locations: []
+            },
+            {   name: "B1" ,
+                locations: []
+            },
+            {   name: "B2" ,
+                locations: []
+            },
+            {   name: "B3" ,
+                locations: []
+            },
+            {   name: "B4" ,
+                locations: []
+            },
+            {   name: "C1" ,
+                locations: []
+            },
+            {   name: "C2" ,
+                locations: []
+            },
+            {   name: "C3" ,
+                locations: []
+            },
+            {   name: "C4" ,
+                locations: []
+            },
+            {   name: "D1" ,
+                locations: []
+            },
+            {   name: "D2" ,
+                locations: []
+            },
+            {   name: "D3" ,
+                locations: []
+            },
+            {   name: "D4" ,
+                locations: []
+            }
+
+        ]);
+
+        res.status(201).json({
+            message: "Zones created",
+            zones: zones,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;

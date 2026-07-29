fb2666ef7f46206a53526aafa7de5cd1b62609e1
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2026-07-29 14:42:10 +0200

    added camera classes in html and camera + zone selection in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,24 @@
 console.log("Frontent loaded");
+
+let selectedCamera = null;
+
+const cameras = document.querySelectorAll(".camera-btn");
+const zones = document.querySelectorAll(".zone");
+
+cameras.forEach((camera) => {
+	camera.addEventListener("click", () => {
+		selectedCamera = camera.dataset.camera;
+		console.log("Selected:", selectedCamera);
+	});
+});
+
+zones.forEach((zone) => {
+	zone.addEventListener("click", () => {
+		if (selectedCamera === null) {
+			console.log("Please select a camera first.");
+			return;
+		}
+
+		console.log(selectedCamera, "placed at", zone.textContent);
+	});
+});

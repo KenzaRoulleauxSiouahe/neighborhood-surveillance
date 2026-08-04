bd93b556d3013d581a69950ca99870425e6e5e37
KenzaRoulleauxSiouahe <kenza.roulleaux.siouahe@student.ehb.be>
2026-08-04 14:39:42 +0200

    added a get route for the locations

--- backend/src/routes/locationGameRoutes.js
+++ backend/src/routes/locationGameRoutes.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const generateLocation = require("../utils/locationGenerator");
+const Location = require("../models/Location");
+
+router.get("/", async (req, res) => {
+	try {
+		const locations = await Location.find();
+
+		res.json(locations);
+	} catch (error) {
+		res.status(500).json({
+			error: error.message,
+		});
+	}
+});
 
 router.post("/generate", async (req, res) => {
 	try {
